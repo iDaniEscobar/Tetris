8262ad64ea7788f9c1cd442d2001059e65381222
Daniela Escobar V <daniela.escobarv@udea.edu.co>
2026-03-16 14:59:30 -0500

    Corrección de tipos de datos y visualización del tablero.
    Se cambian los tipos de datos y se corrige un bucle de impresión.

--- Código/Tetris/main.cpp
+++ Código/Tetris/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int alto, ancho;
+    unsigned short alto, ancho;
 
     cout << "Ingrese el alto del tablero: ";
     cin >> alto;
@@ -12,7 +12,7 @@ int main()
     cout << "Ingrese el ancho del tablero: ";
     cin >> ancho;
 
-    if (alto < 8 || ancho < 8 || (ancho % 8 != 0)) {
+    while (alto < 8 || ancho < 8 || (ancho % 8 != 0)) {
         cout << "Dimensiones no validas. Recuerde que el alto y ancho minimo es 8" << endl<< "y el ancho debe ser multiplo de 8." << endl;
         cout << "Ingrese el alto del tablero: ";
         cin >> alto;
@@ -21,18 +21,39 @@ int main()
         cin >> ancho;
     }
 
-    int anchoBytes = ancho / 8;
+    unsigned char anchoBytes = (unsigned char)(ancho / 8);
 
     unsigned char** tablero = new unsigned char*[alto];
-    for (int i = 0; i < alto; i++) {
+    for (char i = 0; i < alto; i++) {
         tablero[i] = new unsigned char[anchoBytes];
-        for (int j = 0; j < anchoBytes; j++) {
+        for (char j = 0; j < anchoBytes; j++) {
             tablero[i][j] = 0;
         }
     }
 
 
-    for (int i = 0; i < alto; i++) {
+    cout << endl << "Visualizacion del tablero" << endl;
+
+    for (char i = 0; i < alto; i++) {
+        cout << "|";
+
+        for (char j = 0; j < anchoBytes; j++) {
+
+            for (char bit = 7; bit >= 0; bit--) {
+
+                if ((tablero[i][j] >> bit) & 1) {
+                    cout << "[]";
+                } else {
+                    cout << "  ";
+                }
+            }
+        }
+        cout << "|" << endl;
+    }
+
+
+
+    for (char i = 0; i < alto; i++) {
         delete[] tablero[i];
     }
     delete[] tablero;
